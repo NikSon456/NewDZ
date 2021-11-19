581d07a480f874f38a908ea2d35402b35c8121aa
Леви Акерман <83119158+NikSon456@users.noreply.github.com>
2021-11-19 19:16:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// let a = prompt('Ваша зарплата?');
+// b = ((a * 1.15) - (a * 1.15) * 0.1 - 190) / 2
+// b = Math.round(b);
+// console.log(b);
+
+
+for(let i = 1;i <= 7; i++){
+    console.log('')
+    console.log('#'.repeat(i))
+}
